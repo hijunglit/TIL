0623ccced01628815be8cce7170dd4b7b8b6df03
hijunglit <hijunglit@gmail.com>
2023-02-09 14:17:02 +0900

    slider

--- 40-JavaScript/source-code/js/slider.js
+++ 40-JavaScript/source-code/js/slider.js
@@ -1,10 +1,29 @@
-/***** item selector *****/
-const slide = document.querySelector(".slide");
-const prevBtn = document.querySelector(".prev");
+const slideItems = document.querySelectorAll(".slide-item");
 const nextBtn = document.querySelector(".next");
-let slideItems = document.querySelectorAll(".slide-item");
-const maxSlide = slideItems.length;
-const currSlide = 1;
-/***** event listener *****/
+const prevBtn = document.querySelector(".prev");
 
-/***** functions *****/
+// slideItems.forEach(function(slide, index) {
+//     slide.style.left = `${index*100}%`;
+// });
+let counter = 0;
+nextBtn.addEventListener("click", function() {
+    counter++;
+    carousel();
+});
+
+prevBtn.addEventListener("click", function() {
+    counter--;
+    carousel();
+});
+
+function carousel() {
+    counter < slideItems.length -1 ? nextBtn.style.display = "inline" 
+    :nextBtn.style.display = "none";
+    counter > 0 ? prevBtn.style.display = "inline" 
+    : prevBtn.style.display = "none";
+    slideItems.forEach(function(slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+}
+
+prevBtn.style.display = "none";
