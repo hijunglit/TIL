b47f59a1f61b1e9cd36692c18948eaa7bc795a47
hijunglit <hijunglit@gmail.com>
2023-02-20 15:38:08 +0900

    dialog tag for modal

---
+++ 40-JavaScript/source-code/js/dialog.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const dialog = document.querySelector("dialog");
+
+button.addEventListener("click", () => {
+    dialog.showModal();
+});
+dialog.addEventListener("close", () => {
+    console.log(dialog.returnValue);
+})
